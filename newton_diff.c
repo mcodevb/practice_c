0f5219e0b65e395e17d758d9d36cd5327f3a1fe2
mcodevb <getmainul1@gmail.com>
2022-07-06 17:47:48 +0530

    Newton Divided Difference Added

--- newton_diff.c
+++ newton_diff.c
@@ -1,56 +1,55 @@
 #include <stdio.h>
 void main()
 {
-    int n = 4, i, j;
+    int n = 5, i, j;
     float x[5], y[5], diff[5][5], xin, sum, term;
     FILE *fp1, *fp2;
     fp1 = fopen("ndd_input.txt", "r");
     fp2 = fopen("ndd_output.txt", "w");
 
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         fscanf(fp1, "%f", &x[i]);
     }
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         fscanf(fp1, "%f", &y[i]);
     }
-    
+
     fscanf(fp1, "%f", &xin);
 
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         diff[i][0] = y[i];
     }
 
-    for (j = 1; j <= n; j++)
+    for (j = 1; j < n; j++)
     {
-        for (i = 0; i <= n - j; i++)
+        for (i = 0; i < n - j; i++)
         {
             diff[i][j] = (diff[i + 1][j - 1] - diff[i][j - 1]) / (x[i + j] - x[i]);
         }
     }
 
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         fprintf(fp2, " \n");
         fprintf(fp2, "   %12.4f   ", x[i]);
-        for (j = 0; j <= n - i; j++)
+        for (j = 0; j < n - i; j++)
         {
             fprintf(fp2, "   %12.4f   ", diff[i][j]);
         }
-        
     }
 
-        term = 1;
-        sum = diff[0][0];
-        for ( i = 1; i <= n; i++)
-        {
-            term = term*(xin-x[i-1]);
-            sum = sum + term*diff[0][i];
-        }
-        
-         fprintf(fp2, "\n\n     f(%.4f) =  %12.8f  \n ", xin, sum);
+    term = 1;
+    sum = diff[0][0];
+    for (i = 1; i < n; i++)
+    {
+        term = term * (xin - x[i - 1]);
+        sum = sum + term * diff[0][i];
+    }
+
+    fprintf(fp2, "\n\n     f(%.4f) =  %12.8f  \n ", xin, sum);
 
     fclose(fp1);
     fclose(fp2);
