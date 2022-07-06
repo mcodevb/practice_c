a0d92e91d4dd19210dd281b62adc02d610891620
mcodevb <getmainul1@gmail.com>
2022-07-06 18:55:47 +0530

    Romberg and Power added

--- power.c
+++ power.c
@@ -2,39 +2,40 @@
 #include<math.h>
 int main(void)
 {
-    int n, i, j, k, maxiter = 50;
+    int n, i, j, k, maxiter;
 
-    FILE *inputfile, *outputfile;
-    inputfile = fopen("power_input.txt", "r");
-    outputfile = fopen("power_output.txt", "w");
+    FILE *fp1, *fp2;
+    fp1 = fopen("power.dat", "r");
+    fp2 = fopen("power.out", "w");
 
     // Input matrix dimension
-    fscanf(inputfile, "%d", &n);
-    float a[n][n], x[n], xnew[n], lambda_old = 1.0, lambda_new, tol = 1e-5;
+    fscanf(fp1, "%d %d", &n, &maxiter);
+    float a[n][n], x[n], xnew[n], lambda_old = 1.0, lambda_new, tol = 1e-7; // 6 decimal place: 0.5e-6
 
     // scan matrix
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
-            fscanf(inputfile, "%f", &a[i][j]);
+            fscanf(fp1, "%f", &a[i][j]);
 
     // scan the initial guess vector
     for (i = 0; i < n; i++)
-        fscanf(inputfile, "%f", &x[i]);
+        fscanf(fp1, "%f", &x[i]);
 
     // print matrix  on the terminal
+    fprintf(fp2, "\n Given Matrix A = \n\t");
     for (int ii = 0; ii < n; ii++)
     {
         for (int jj = 0; jj < n; jj++)
         {
-            printf("%f ", a[ii][jj]);
+            fprintf(fp2,"%f ", a[ii][jj]);
         }
-        printf("\n");
+        fprintf(fp2,"\n\t");
     }
 
-    printf("\n b = \n");
+    fprintf(fp2, "\ninitial guess vector = \n");
     for (int ii = 0; ii < n; ii++)
     {
-        printf("   %f\n", x[ii]);
+        fprintf(fp2, "%f\n", x[ii]);
     }
 
     for (int iter = 1; iter <= maxiter; iter++)
@@ -48,17 +49,17 @@ int main(void)
             }
             xnew[i] = sum;
         }
-        // find maximum element in xnew
 
+        // find maximum (in magnitude) element in xnew
         lambda_new = fabs(xnew[0]);
         for (i = 1; i < n; i++)
         {
-            if (fabs(xnew[i] > lambda_new))
+            if (fabs(xnew[i]) > lambda_new)
             {
                 lambda_new = fabs(xnew[i]);
             }
         }
-
+        // normalize the xnew vector by dividing with the lambda_new
         for (i = 0; i < n; i++)
         {
             x[i] = xnew[i] / lambda_new;
@@ -71,9 +72,14 @@ int main(void)
     }
 
     // print solution in output file
-    fprintf(outputfile, "Dominant eigenvalue: %f\n ", lambda_new);
-
-    fclose(inputfile);
-    fclose(outputfile);
+    fprintf(fp2, "\n\nDominant eigenvalue: %f\n ", lambda_new);
+   // print the eigenvector
+   fprintf(fp2, "\n Eigenvector = \n");
+    for (int ii = 0; ii < n; ii++)
+    {
+        fprintf(fp2, "%f\n", x[ii]);
+    }
+    fclose(fp1);
+    fclose(fp2);
     return 0;
 }
--- romberg.c
+++ romberg.c
@@ -2,31 +2,31 @@
 #include <math.h>
 float f(float x)
 {
-  return 1.0 / (x * x + 1);
+  return exp(-x*x);
 }
 void main()
 {
   FILE *fp1, *fp2;
-  float a, b, h, T[10][10], sum;
+  float a, b, h, T[20][20], sum;
   int i, j, k, m, n, w;
-  fp1 = fopen("romberg_input.txt", "r");
-  fp2 = fopen("romberg_output.txt", "w");
+  fp1 = fopen("romberg.dat", "r");
+  fp2 = fopen("romberg.out", "w");
   fscanf(fp1, "%f %f %d", &a, &b, &n);
-  h = b - a;
-  m = 1;
-  T[0][0] = (h / 2.0) * (f(a) + f(b));
-  for (i = 1; i < n; i++)
+  h = b - a; // initial step size for Trapezoidal rule
+  m = 1;    
+  T[0][0] = (h / 2.0) * (f(a) + f(b)); // initial estimate using Trapezoidal rule
+  for (i = 1; i < n; i++) // Trapezoidal rule for h/(2^n)
   {
-    h = h / 2.0;
-    m = 2 * m;
+    h = h / 2.0; // when step size is halved
+    m = 2 * m;   // number of intervals gets doubled
     sum = 0.5 * T[i - 1][0];
-    for (k = 1; k < m; k = k + 2)
+    for (k = 1; k < m; k = k + 2)  //  odd points
     {
       sum = sum + h * f(a + k * h);
     }
     T[i][0] = sum;
   }
-
+  //
   for (j = 1; j < n; j++)
     for (i = j; i < n; i++)
       T[i][j] = (pow(4.0, j) * T[i][j - 1] - T[i - 1][j - 1]) / (pow(4.0, j) - 1.0);
@@ -35,9 +35,9 @@ void main()
   {
     for (j = 0; j <= i; j++)
       fprintf(fp2, "%f  ", T[i][j]);
-    fprintf(fp2, "\n");
+      fprintf(fp2, "\n");
   }
-
+  fprintf(fp2,"\n Romberg estimate of integration =%f\n",T[n-1][n-1]);
   fclose(fp1);
   fclose(fp2);
 }
